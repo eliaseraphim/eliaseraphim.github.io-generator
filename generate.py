42c38cd1b1eccb20c1663d31ddf857eef9e8bb97
Elia Deppe <elia.deppe7@gmail.com>
2023-01-08 19:13:55 -0500

    Updated generate and pages to properly render.

--- generate.py
+++ generate.py
@@ -1,29 +1,39 @@
 import os
 from jinja2 import Environment, FileSystemLoader
 
+FILES = {
+    'output/index.html': 'pages/index.html',
+    'output/projects.html': 'pages/projects.html',
+}
+
 PATH = os.path.dirname(os.path.abspath(__file__))
+OUTPUT_PATH = os.path.join(PATH, 'output')
+
 TEMPLATE_ENVIRONMENT = Environment(
-    autoescape=False,
+    autoescape=True,
     loader=FileSystemLoader(os.path.join(PATH, 'templates')),
 )
 
-def render_template(template_filename, context):
-    return TEMPLATE_ENVIRONMENT.get_template(template_filename).render(context)
+def clear():
+    output_directory = os.listdir(OUTPUT_PATH)
+    for item in output_directory:
+        if item.endswith('.html'):
+            os.remove(os.path.join(OUTPUT_PATH, item))
 
+def render_template(filename, context):
+    return TEMPLATE_ENVIRONMENT.get_template(filename).render(context)
 
-def create_index_html():
-    filename = "output/output.html"
-    context = {
-        'sidebar': TEMPLATE_ENVIRONMENT.get_template('structure/sidebar.html')
-    }
-
-    with open(filename, 'w') as file:
-        html = render_template('pages/index.html', context)
-        file.write(html)
+def create_site():
+    for filename in FILES:
+        with open(filename, 'w') as file:
+            html = render_template(FILES[filename], {})
+            file.write(html)
 
 
 def main():
-    create_index_html()
+    clear()
+    create_site()
+
 
 
 if __name__=='__main__':
